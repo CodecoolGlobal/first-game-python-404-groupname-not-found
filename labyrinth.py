bea22a8e3a26fbdb861e47c3450c654ba0aac64e
Krisztian Pometko <krisztian@pometko.hu>
2019-07-29 14:53:23 +0200

    Removing global variables

--- labyrinth.py
+++ labyrinth.py
@@ -5,6 +5,7 @@ playerX = 0
 playerY = 0
 won = False
 
+
 def main():
     global playerX
     global playerY
@@ -13,69 +14,80 @@ def main():
     playerX = pCoords[1]
     playerY = pCoords[0]    
     
-    game(map)
+    gameState = {
+        "playerX": pCoords[1],
+        "playerY": pCoords[0],
+        "map": map,
+        "won": False
+        }
 
-def move(dir, maze): #Moves player character bz 1 tile in the given direction
-    global playerY
-    global playerX, won
+    game(gameState)
+
+
+def move(dir, gameState):  # Moves player character bz 1 tile in the given direction
+    maze = gameState["map"]
+    playerY = gameState["playerY"]
+    playerX = gameState["playerX"]
     dir = direction(dir)
-    if  maze[playerY+dir[0]][playerX+dir[1]] != 'X':
+    if maze[playerY+dir[0]][playerX+dir[1]] != 'X':
         maze[playerY][playerX] = ' '
         playerX += dir[1]
         playerY += dir[0]
         if maze[playerY][playerX] == "O":
-            won = True
-        maze[playerY][playerX]  = '@'        
-    return maze
+            gameState["won"] = True
+        maze[playerY][playerX] = '@'
+    
+    gameState["playerY"] = playerY
+    gameState["playerX"] = playerX
+    
+    return gameState
 
-def direction(dir): #Converts direction string to vector 
+
+def direction(dir):  # Converts direction string to vector 
     if dir == "up":
-        return -1,0
+        return -1, 0
     elif dir == "down":
-        return 1,0
+        return 1, 0
     elif dir == "left":
-        return 0,-1
+        return 0, -1
     elif dir == "right":
-        return 0,1
+        return 0, 1
     else:
-        return 0,0 
+        return 0, 0 
 
 
-def game(map): #display and user input
+def game(gameState):  # display and user input
     try:
         screen = curses.initscr()
         curses.cbreak() 
         screen.keypad(1)
         curses.noecho()
         key = ''
-        screen.addstr(0,0, lab.printmap(map))
-        while key != curses.KEY_END: #End key ends the program
-            global won
+        screen.addstr(0, 0, lab.printmap(gameState["map"]))
+        while key != curses.KEY_END:  # End key ends the program
             key = screen.getch()
-            if not won:
+            if not gameState["won"]:
                 screen.refresh()
                 if key == curses.KEY_UP:
-                    map = move("up",map)
+                    gameState = move("up", gameState)
                 elif key == curses.KEY_DOWN:
-                    map = move("down",map)            
+                    gameState = move("down", gameState)            
                 elif key == curses.KEY_LEFT:
-                    map = move("left",map)
+                    gameState = move("left", gameState)
                 elif key == curses.KEY_RIGHT:
-                    map = move("right",map)
-                else:
-                    pass 
-            if won == True:
+                    gameState = move("right", gameState)
+            if gameState["won"]:
                 text = lab.victory()
                 for lines in range(len(text)):
                     screen.addstr(8+lines, (80-len(text[0]))//2, text[lines].rstrip(), curses.A_BLINK+curses.COLOR_BLUE)
             else:
-                screen.addstr(0,0, lab.printmap(map))
+                screen.addstr(0, 0, lab.printmap(gameState["map"]))
             screen.refresh()
     except:
+        curses.endwin()
         print("Terminal is too small")
     curses.endwin()
 
 
-
-if __name__ ==  "__main__":
+if __name__ == "__main__":
     main()
