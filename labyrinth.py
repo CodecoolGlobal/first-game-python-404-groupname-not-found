5a93c3edcb74ebf4d9687304f0e54b64d4031b65
Krisztian Pometko <krisztian@pometko.hu>
2019-07-31 08:52:42 +0200

    Seperate gui mode from ascii version

--- labyrinth.py
+++ labyrinth.py
@@ -1,9 +1,10 @@
 import lab
 import curses
 import pygame
+import sys
 
 
-def main():
+def main(arg):
     map = lab.openmap("map.txt")
     pCoords = lab.atplace(map)
     gameState = {
@@ -12,7 +13,10 @@ def main():
         "map": map,
         "won": False
         }
-    game(gameState)
+    if len(arg) >= 2 and arg[1] == "-gui":
+        gameGui(gameState)
+    else:
+        game(gameState)
 
 
 def move(dir, gameState):
@@ -48,10 +52,33 @@ def direction(dir):  # Converts direction string to vector
         return 0, 0
 
 
-def game(gameState):  # display and user input
+def gameGui(gameState):  # display and user input
+    running = True
     try:
- 
         pygame.init()
+        clock = pygame.time.Clock()
+        while running:
+            clock.tick(12)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            pressed = pygame.key.get_pressed()
+            if pressed[pygame.K_UP]:
+                gameState = move("up", gameState)
+            if pressed[pygame.K_DOWN]:
+                gameState = move("down", gameState)
+            if pressed[pygame.K_LEFT]:
+                gameState = move("left", gameState)
+            if pressed[pygame.K_RIGHT]:
+                gameState = move("right", gameState)
+            drawMap(gameState, pygame)
+
+    except Exception as e:
+        print(f"Something went wrong..\n{e}")
+
+
+def game(gameState):
+    try:
         screen = curses.initscr()
         curses.cbreak()
         screen.keypad(1)
@@ -59,7 +86,6 @@ def game(gameState):  # display and user input
         key = ''
         screen.addstr(0, 0, lab.printmap(gameState["map"]))
         while key != curses.KEY_END:  # End key ends the program
-            drawMap(gameState, pygame)
             key = screen.getch()
             if not gameState["won"]:
                 screen.refresh()
@@ -92,16 +118,22 @@ def drawMap(gameState, pygame):
     map = gameState["map"]
     black = (0, 0, 0)
     display_surface = pygame.display.set_mode(((len(map[0])-1)*15, len(map)*15))
-    display_surface.fill((255, 255, 255)) 
-    pygame.draw.rect(display_surface, (255, 0, 0), (15*gameState["playerX"], 15*gameState["playerY"], 15, 15))
+    display_surface.fill((255, 255, 255))
+    pygame.draw.rect(display_surface,
+                     (255, 0, 0),
+                     (15*gameState["playerX"], 15*gameState["playerY"], 15, 15)
+                     )
     for row in range(len(map)):
         for col in range(len(map[row])):
             if map[row][col] == "X":
                 pygame.draw.rect(display_surface, black, (15*col, 15*row, 15, 15))
             elif map[row][col] == "O":
-                pygame.draw.rect(display_surface, (255, 0, 255), (15*col, 15*row, 15, 15))
+                pygame.draw.rect(display_surface,
+                                 (255, 0, 255), (15 * col, 15 * row, 15, 15)
+                                 )
     pygame.display.update()
     return display_surface
 
+
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
